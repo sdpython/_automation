c4a57b3e288e0a27ca8e0227a13303dc76036a16
xavier dupré <xavier.dupre@gmail.com>
2017-10-29 21:42:38 +0100

    update script

--- notebook/run_pyspark_notebook.bat
+++ notebook/run_pyspark_notebook.bat
@@ -3,14 +3,17 @@ if "%1"=="" goto default_value_spark:
 set local_pyspark="%1"
 goto custom_spark:
 :default_value_spark:
-set local_pyspark=c:\%USERNAME%\spark\spark-2.0.2-bin-hadoop2.7
+set local_pyspark=c:\%USERNAME%\spark-2.2.0-bin-hadoop2.7
+if not exist %local_pyspark% set local_pyspark=C:\xavierdupre\spark-2.2.0-bin-hadoop2.7
+set local_user=%USERNAME%
+if not exist %local_user% set local_user=xavierdupre
 :custom_spark:
 
 if "%2"=="" goto default_value_python:
 set local_python="%2"
 goto custom_python:
 :default_value_python:
-set local_python=c:\Python35_x64
+set local_python=c:\Python36_x64
 :custom_python:
 
 set CURRENT=%~dp0
@@ -27,12 +30,19 @@ set PATH=%PATH%;%local_pyspark%\bin
 set PYSPARK_PYTHON=%local_python%\python
 set SPARK_HIVE=true
 
+if not exist %SPARK_HOME% goto error:
+goto next:
+:error:
+@echo Unable to find '%SPARK_HOME%'.
+exit /b 1
+
+:next:
 @echo HADOOP_HOME=%HADOOP_HOME%
 @echo PYTHONPATH=%PYTHONPATH%
 @echo PYSPARK_PYTHON=%PYSPARK_PYTHON%
 @echo SPARK_HIVE=%SPARK_HIVE%
 @echo SPARK_HOME=%SPARK_HOME%
-set PYTHONPATH=%PYTHONPATH%;%current%\src;%current%\..\pyquickhelper\src;%current%\..\jyquickhelper\src;%current%\..\pymmails\src;%current%\..\pyensae\src;%current%\..\pyrsslocal\src;%current%\..\pymyinstall\src;%current%\..\mlstatpy\src
+set PYTHONPATH=%PYTHONPATH%;%current%\src;%current%\..\..\pyquickhelper\src;%current%\..\..\jyquickhelper\src;%current%\..\..\pymmails\src;%current%\..\..\pyensae\src;%current%\..\..\pyrsslocal\src;%current%\..\..\pymyinstall\src;%current%\..\..\mlstatpy\src
 
 :wintutils:
 winutils.exe chmod -R 777 \tmp\hive
