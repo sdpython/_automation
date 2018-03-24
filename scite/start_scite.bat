33e59ad68bc8b61bce83d1071bb4093b909ffd3f
xavier dupré <xavier.dupre@gmail.com>
2018-03-24 21:59:59 +0100

    add scite

---
+++ scite/start_scite.bat
@@ -0,0 +1,9 @@
+@echo off
+set CURRENT=%~dp0..\
+set PYADDPATH=%CURRENT%
+@echo [ENV] PYADDPATH=%PYADDPATH%
+set PYTHONPATH=%PYADDPATH%\pyquickhelper\src;%PYADDPATH%\pyensae\src;%PYADDPATH%\pyrsslocal\src;%PYADDPATH%\jyquickhelper\src
+set PYTHONPATH=%CSADDPATH%\pyscope\src;%CSADDPATH%\pyaether\src;%CSADDPATH%\scikit-tlc\src;%CSADDPATH%\pyscopeslapi\src;%CSADDPATH%\pyscopeml\src;%PYTHONPATH%
+set PYTHONPATH=%CURRENT%\automation\pytlccontrib\src;%PYTHONPATH%
+set PATH=c:\Python364_x64;c:\Python364_x64;Scripts;%PATH%
+start /b %CURRENT%\scite\wscite\SciTE.exe
