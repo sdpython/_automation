43a0ca9c1bc75be8a1c0ac0342d7fc1acf8a3d67
xavier dupré <xavier.dupre@gmail.com>
2019-01-09 18:51:07 +0100

    Update machinelearning.sh

--- fork/machinelearning.sh
+++ fork/machinelearning.sh
@@ -9,31 +9,21 @@ echo --BUILD--DEBUG--
 bash build.sh -debug || exit 1
 
 echo --UNITTEST--RELEASE--
+dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Analyzer" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Benchmarks" || exit 1
-dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.CodeAnalyzer" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Core" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.CpuMath" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.FSharp" || exit 1
-dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.OnnxTransformTest" || exit 1
-dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Predictor" || exit 1
+dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.EntryPoints" || exit 1
+dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.InteralCodeAnalyzer" || exit 1
+dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.RunTests" || exit 1
+dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Scenarios" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.StaticPipelineTesting" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Sweeper" || exit 1
-dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.TestFramework" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.Tests" || exit 1
 dotnet test Microsoft.ML.sln -c Release --filter "FullyQualifiedName~Microsoft.ML.TimeSeries" || exit 1
 echo --UNITTEST--DEBUG--
 dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.Benchmarks" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.CodeAnalyzer" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.Core" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.CpuMath" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.FSharp" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.OnnxTransformTest" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.Predictor" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.StaticPipelineTesting" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.Sweeper" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.TestFramework" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.Tests" || exit 1
-dotnet test Microsoft.ML.sln -c Debug --filter "FullyQualifiedName~Microsoft.ML.TimeSeries" || exit 1
 
 echo --END--
 cd ..
