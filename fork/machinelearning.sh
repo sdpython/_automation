7cab89d53e5a62d9395d6d37663294b3ddc88a46
xavier dupré <xavier.dupre@gmail.com>
2019-01-09 01:33:22 +0100

    Update machinelearning.sh

--- fork/machinelearning.sh
+++ fork/machinelearning.sh
@@ -5,10 +5,13 @@ cd machinelearning
 
 echo --BUILD--
 bash build.sh -release
+if [ $? -ne 0 ]; then exit $?; fi
 bash build.sh -debug
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --UNITTEST--
 bash build.sh -runTests -Release
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --END--
 cd ..
