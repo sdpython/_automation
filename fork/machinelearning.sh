14b96a50cc5aa6d625d307a2b8e6d72c8b5528c2
xavier dupré <xavier.dupre@gmail.com>
2019-01-09 17:43:52 +0100

    Update machinelearning.sh

--- fork/machinelearning.sh
+++ fork/machinelearning.sh
@@ -9,9 +9,9 @@ echo --BUILD--DEBUG--
 bash build.sh -debug || exit 1
 
 echo --UNITTEST--RELEASE--
-dotnet test Microsoft.sln -c Release || exit 1
+dotnet test Microsoft.ML.sln -c Release || exit 1
 echo --UNITTEST--DEBUG--
-dotnet test Microsoft.sln -c Debug || exit 1
+dotnet test Microsoft.ML.sln -c Debug || exit 1
 
 echo --END--
 cd ..
