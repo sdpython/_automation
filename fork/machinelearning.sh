809de2c6f6dd53a3c2bcf5b4d9d240c1e8851b88
xavier dupré <xavier.dupre@gmail.com>
2019-01-09 02:01:41 +0100

    Update machinelearning.sh

--- fork/machinelearning.sh
+++ fork/machinelearning.sh
@@ -3,12 +3,15 @@ echo --CLONE--
 git clone -b ext --single-branch https://github.com/sdpython/machinelearning.git --recursive
 cd machinelearning
 
-echo --BUILD--
+echo --BUILD--RELEASE--
 bash build.sh -release || exit 1
+echo --BUILD--DEBUG--
 bash build.sh -debug || exit 1
 
-echo --UNITTEST--
+echo --UNITTEST--RELEASE--
 bash build.sh -runTests -Release || exit 1
+echo --UNITTEST--DEBUG--
+bash build.sh -runTests -Debug || exit 1
 
 echo --END--
 cd ..
