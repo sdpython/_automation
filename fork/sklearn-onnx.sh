194606261db9882f1df4a150ddf4b4602b5c67e2
xavier dupré <xavier.dupre@gmail.com>
2019-03-10 13:17:24 +0100

    add script for skl2onnx

---
+++ fork/sklearn-onnx.sh
@@ -0,0 +1,15 @@
+echo --CLONE--
+git clone -b ext --single-branch https://github.com/xadupre/sklearn-onnx.git --recursive
+cd sklearn-onnx
+
+echo --TEST--
+python3.7 -m pytest tests
+
+echo --DOCUMENTATION--
+python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])"
+
+echo --COVERAGE--
+python3.7 -m coverage run --include=skl2onnx/** tests/benchmark.py
+
+echo --END--
+cd ..
