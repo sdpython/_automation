2889c018b2552ebb8703b3b053c466e29dd9acf4
xavier dupré <xavier.dupre@gmail.com>
2019-03-10 18:56:04 +0100

    update main page

--- jenkins/publish_lectures.py
+++ jenkins/publish_lectures.py
@@ -97,8 +97,17 @@ for i, mod in enumerate(sorted(modules)):
     print("  {0}/{1}: {2}".format(i + 1, len(modules), mod))
 
 ##################
-# La fonction :func:`publish_teachings_to_web cache` cache beaucoup de chose.
+# La fonction :func:`publish_teachings_to_web cache` cache beaucoup de choses.
+
+other_projects = []
+for name in ['scikit-onnxruntime', 'sklearn-onnx']:
+    folder = "/var/lib/jenkins/workspace/_automation/_automation_FORK_{0}_37_std/{0}".format(name)
+    root_web = "/www/htdocs/app/%s/helpsphinx" % name
+    other_projects.append(dict(status_file="status_projects_%s.txt" % name),
+                               root_local=folder, root_web=root_web)) 
+    
 publish_teachings_to_web(login=user, ftpsite=ftpsite, google_id=google_id,
                          location=location, rootw=rootw, rootw2=rootw2,
                          modules=modules, password=pwd, suffix=suffix,
-                         force_allow=["xavierdupre"], exc=False)
+                         force_allow=["xavierdupre"], exc=False,
+                         additional_projects=other_projects)
