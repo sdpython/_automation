a9c6146a8fa481fda6da8f725150f1ce2a7fa178
xavier dupré <xavier.dupre@gmail.com>
2019-03-10 14:02:11 +0100

    update scripts

--- fork/keras-onnx.sh
+++ fork/keras-onnx.sh
@@ -4,9 +4,11 @@ cd sklearn-onnx
 
 echo --TEST--
 python3.7 -m pytest tests
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --DOCUMENTATION--
 python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])"
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --END--
 cd ..
--- fork/sklearn-onnx.sh
+++ fork/sklearn-onnx.sh
@@ -4,12 +4,15 @@ cd sklearn-onnx
 
 echo --TEST--
 python3.7 -m pytest tests
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --DOCUMENTATION--
 python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])"
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --COVERAGE--
 python3.7 -m coverage run --include=skl2onnx/** tests/benchmark.py
+if [ $? -ne 0 ]; then exit $?; fi
 
 echo --END--
 cd ..
