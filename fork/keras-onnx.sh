f5e5f4c498d90c64216a42049529bfc0d0f36e36
xavier dupré <xavier.dupre@gmail.com>
2019-03-10 16:47:58 +0100

    update script

--- fork/keras-onnx.sh
+++ fork/keras-onnx.sh
@@ -5,6 +5,12 @@ cd keras-onnx
 echo --TEST--
 python3.7 -m pytest tests || exit 1
 
+echo --WHEEL--
+python3.7 -u setup.py bdist_wheel || exit 1
+
+echo --COPY--
+cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
+
 echo --DOCUMENTATION--
 python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
 
--- fork/sklearn-onnx.sh
+++ fork/sklearn-onnx.sh
@@ -1,9 +1,15 @@
 echo --CLONE--
-git clone -b master --single-branch https://github.com/xadupre/sklearn-onnx.git --recursive
+git clone -b voting2 --single-branch https://github.com/xadupre/sklearn-onnx.git --recursive
 cd sklearn-onnx
 
 echo --TEST--
 python3.7 -m pytest tests || exit 1
+
+echo --WHEEL--
+python3.7 -u setup.py bdist_wheel || exit 1
+
+echo --COPY--
+cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
 
 echo --DOCUMENTATION--
 python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
