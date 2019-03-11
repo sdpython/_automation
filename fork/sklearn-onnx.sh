3aaf24fdd1a940f25f06d54b7bbbe7ac53fd7a74
xavier dupré <xavier.dupre@gmail.com>
2019-03-11 12:53:32 +0100

    Update sklearn-onnx.sh

--- fork/sklearn-onnx.sh
+++ fork/sklearn-onnx.sh
@@ -11,15 +11,17 @@ python3.7 -u setup.py bdist_wheel || exit 1
 echo --COPY--
 cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
 
-echo --DOCUMENTATION--
-python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
-
 echo --COVERAGE--
 cd tests
 python3.7 -m coverage run benchmark.py || exit 1
 python3.7 -m coverage html -d ../dist/html/coverage_html || exit 1
 cp TESTDUMP/*.xlsx ../dist/html
 cd ..
+
+echo --DOCUMENTATION--
+mkdir docs/coverage_html || exit 1
+cp -r dist/html/coverage_html docs/coverage_html || exit 1
+python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
 
 echo --END--
 cd ..
