ebb73b82f9558fcf97f32e5edaaaed2e24130968
xavier dupré <xavier.dupre@gmail.com>
2019-04-04 09:07:29 +0200

    Update onnxmltools.sh

--- fork/onnxmltools.sh
+++ fork/onnxmltools.sh
@@ -6,7 +6,7 @@ echo --INSTALL--
 pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnxruntime onnx || exit 1
 
 echo --INSTALL-KERAS--
-pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ keras-onnx || exit 1
+pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ keras2onnx || exit 1
 
 echo --WHEEL--
 python3.7 -u setup.py bdist_wheel || exit 1
