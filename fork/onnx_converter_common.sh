d71e32b534f0d9365eed5b3b0b2eb30ce5359eb1
xavier dupré <xavier.dupre@gmail.com>
2019-04-11 18:04:29 +0200

    update scripts

--- fork/onnx_converter_common.sh
+++ fork/onnx_converter_common.sh
@@ -1,12 +1,9 @@
 echo --CLONE--
 git clone -b master --single-branch https://github.com/xadupre/onnxmltools.git --recursive
-cd onnxmltools
+cd onnxmltools/onnxutils
 
 echo --INSTALL--
-pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnxruntime onnx onnxtk || exit 1
-
-echo --INSTALL-KERAS--
-pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ keras2onnx || exit 1
+pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ onnxruntime onnx || exit 1
 
 echo --TEST--
 python3.7 -m pytest tests || exit 1
@@ -17,11 +14,8 @@ python3.7 -u setup.py bdist_wheel || exit 1
 echo --COPY--
 cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
 
-echo --INSTALL--
-pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx scikit-onnxruntime || exit 1
-
 echo --DOCUMENTATION--
 python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
 
 echo --END--
-cd ..
+cd ../..
