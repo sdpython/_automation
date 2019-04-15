5dd6e1f07ccb4444db86acfa781c2351840ce544
xavier dupré <xavier.dupre@gmail.com>
2019-04-15 12:50:25 +0200

    Update onnx_converter_common.sh

--- fork/onnx_converter_common.sh
+++ fork/onnx_converter_common.sh
@@ -14,8 +14,5 @@ python3.7 -u setup.py bdist_wheel || exit 1
 echo --COPY--
 cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
 
-echo --DOCUMENTATION--
-python3.7 -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
-
 echo --END--
 cd ../..
