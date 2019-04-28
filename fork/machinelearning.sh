339ef3b04de6052c80294aaa5d6e4d181695f596
xavier dupré <xavier.dupre@gmail.com>
2019-04-28 10:46:35 +0200

    Update machinelearning.sh

--- fork/machinelearning.sh
+++ fork/machinelearning.sh
@@ -3,6 +3,7 @@
 # It must be manually built and the job can started again.
 # After the first execution, chmod -R 755 * must be run.
 echo --CLONE--
+export
 git clone -b master --single-branch https://github.com/sdpython/machinelearning.git --recursive
 cd machinelearning
 git pull
