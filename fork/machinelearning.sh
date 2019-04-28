d68b6a8a5a99306fa33b006a378f5b0dfe020669
xavier dupré <xavier.dupre@gmail.com>
2019-04-28 12:42:56 +0200

    Update machinelearning.sh

--- fork/machinelearning.sh
+++ fork/machinelearning.sh
@@ -4,7 +4,7 @@
 # After the first execution, chmod -R 755 * must be run.
 echo --CLONE--
 export
-git clone -b master --single-branch https://github.com/sdpython/machinelearning.git --recursive
+git clone -b master --single-branch https://github.com/dotnet/machinelearning.git --recursive
 cd machinelearning
 git pull
 git submodule update --init --recursive
