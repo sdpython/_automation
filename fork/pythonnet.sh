3b91cca29c7d166ec965611daacccc11dc246a8e
xavier dupré <xavier.dupre@gmail.com>
2019-04-29 18:54:31 +0200

    Create pythonnet.sh

---
+++ fork/pythonnet.sh
@@ -0,0 +1,27 @@
+echo --CLONE--
+git clone -b nomo --single-branch https://github.com/xadupre/pythonnet.git --recursive
+cd pythonnet
+
+echo --UPDATE--
+git pull
+
+echo --UPDATE-SUBMODULE--
+git submodule update --init --recursive
+
+echo --BUILD--
+python3.7 -u setup.py build_ext --inplace
+
+echo --WHEEL--
+python3.7 -u setup.py bdist_wheel
+
+echo --TEST--
+dotnet test pythonnet.15.sln
+
+echo --TESTPY--
+python3.7 -m pytest
+
+echo --COPY--
+cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server || exit 1
+
+echo --END--
+cd ..
