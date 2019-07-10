c8649e48adf6f11a95dda0c4d763aa71b5275871
xavier dupré <xavier.dupre@gmail.com>
2019-07-10 17:12:21 +0200

    update jobs

---
+++ fork/sklearn-onnx_version_ort_dev.sh
@@ -0,0 +1,8 @@
+echo --INSTALL--
+pip3.7 install --no-cache-dir --no-deps --index http://localhost:8067/simple/ onnxruntime onnx onnxconverter-common sklearn-onnx mlprodict || exit 1
+
+echo --TEST-DEV--
+python3.7 -m mlprodict validate_runtime -v 1 -o 7 -c 1 -r onnxruntime1 -out bench_raw_onnxruntime.xlsx -out bench_onnxruntime.xlsx -b 1 -ve 1 || exit 1
+
+echo --END--
+cd ..
