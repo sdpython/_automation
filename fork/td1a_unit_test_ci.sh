040928d093e68b7c7b818033cd7012bffc8aae95
xavier dupré <xavier.dupre@gmail.com>
2019-09-15 15:04:43 +0200

    update automation

---
+++ fork/td1a_unit_test_ci.sh
@@ -0,0 +1,16 @@
+echo --CLONE--
+git clone -b master --single-branch https://github.com/sdpython/td1a_unit_test_ci.git --recursive
+cd td1a_unit_test_ci
+
+echo --TEST--
+python3.7 -m coverage run  --omit=tests/test_*.py -m unittest discover tests || exit 1
+python3.7 -m coverage html -d test-reports/coverage.html || exit 1
+
+echo --WHEEL--
+python3.7 -u setup.py bdist_wheel || exit 1
+
+echo --DOC--
+python3.7 -m sphinx build -M html doc test-reports
+
+echo --END--
+cd ..
