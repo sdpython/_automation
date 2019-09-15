fea051340ff248f6c5b1cf55a93a7b26e1bd8e2e
xavier dupré <xavier.dupre@gmail.com>
2019-09-15 15:12:26 +0200

    Update td1a_unit_test_ci.sh

--- fork/td1a_unit_test_ci.sh
+++ fork/td1a_unit_test_ci.sh
@@ -4,13 +4,13 @@ cd td1a_unit_test_ci
 
 echo --TEST--
 python3.7 -m coverage run  --omit=tests/test_*.py -m unittest discover tests || exit 1
-python3.7 -m coverage html -d test-reports/coverage.html || exit 1
+python3.7 -m coverage html -d dist/html/coverage.html || exit 1
 
 echo --WHEEL--
 python3.7 -u setup.py bdist_wheel || exit 1
 
 echo --DOC--
-python3.7 -m sphinx build -M html doc test-reports
+python3.7 -m sphinx build -b html doc dist/html || exit 1
 
 echo --END--
 cd ..
