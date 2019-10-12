39f4f50d9824da0f3123e1b51734904650219b5d
xavier dupré <xavier.dupre@gmail.com>
2019-10-12 09:33:42 +0200

    add asv

---
+++ fork/asv-jenkins.sh
@@ -0,0 +1,12 @@
+echo --CLONE--
+git clone -b jenkins --single-branch https://github.com/sdpython/asv.git --recursive
+cd asv
+
+echo --BUILD--
+python3.7 -u setup.py build_ext --inplace || exit 1
+
+echo --WHEEL--
+python3.7 -u setup.py bdist_wheel || exit 1
+
+echo --COPY--
+cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
