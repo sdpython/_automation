777b2dafa5295e6dda4675f2e854ff0297420424
xavier dupré <xavier.dupre@gmail.com>
2019-11-18 18:06:12 +0100

    Update publish_lectures.py

--- jenkins/publish_lectures.py
+++ jenkins/publish_lectures.py
@@ -192,9 +192,21 @@ if os.path.exists(folder):
 else:
     print("[] Unable to find '{}'.".format(folder))
 
-# benchmark scikit-learn CPP full
+# benchmark scikit-learn CPP
 folder = "/var/lib/jenkins/workspace/_benchmarks/_benchmarks_SKLBENCHONNX_CPP_37_std/asv-skl2onnx/html"
 name = "asv-skl2onnx-cpp"
+if os.path.exists(folder):
+    root_web = (root_template2 % ('benches', name))
+    other_projects.append(dict(status_file="status_projects_benches_%s.txt" % name,
+                               local="scikit-learn_benchmarks_full", root_web=root_web,
+                               root_local=folder))
+    print("+ publish '{}'".format(folder))
+else:
+    print("[] Unable to find '{}'.".format(folder))
+
+# benchmark scikit-learn CPP SAME
+folder = "/var/lib/jenkins/workspace/_benchmarks/_benchmarks_SKLBENCHONNX_CPP_SAME_37_std/asv-skl2onnx/html"
+name = "asv-skl2onnx-cpp-same"
 if os.path.exists(folder):
     root_web = (root_template2 % ('benches', name))
     other_projects.append(dict(status_file="status_projects_benches_%s.txt" % name,
