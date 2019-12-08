7f73800c521607573af926d1f90826ab68addbc1
xavier dupré <xavier.dupre@gmail.com>
2019-12-08 11:41:32 +0100

    add job

---
+++ fork/td3a_cpp.sh
@@ -0,0 +1,17 @@
+echo --CLONE--
+git clone -b master --single-branch https://github.com/sdpython/td3a_cpp.git --recursive
+cd td3a_cpp
+
+echo --TEST--
+python3.7 -m coverage run  --omit=tests/test_*.py -m unittest discover tests || exit 1
+python3.7 -m coverage html -d dist/html/coverage.html --include **/td3a_cpp/** || exit 1
+python3.7 -m flake8 . || exit 1
+
+echo --WHEEL--
+python3.7 -u setup.py bdist_wheel || exit 1
+
+echo --DOC--
+python3.7 -m sphinx -b html doc dist/html || exit 1
+
+echo --END--
+cd ..
