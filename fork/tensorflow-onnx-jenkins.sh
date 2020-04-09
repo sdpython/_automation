0ee0bd9cb0d7cde6baf6e314dd2a24f0268c8086
xavier dupré <xavier.dupre@gmail.com>
2020-04-09 11:13:24 +0200

    update CI

---
+++ fork/tensorflow-onnx-jenkins.sh
@@ -0,0 +1,22 @@
+echo --CLONE--
+git clone -b jenkins --single-branch https://github.com/xadupre/tensorflow-onnx.git --recursive
+cd keras-onnx
+
+echo --INSTALL--
+pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnx onnxruntime_dnnl onnxconverter-common || exit 1
+
+echo --WHEEL--
+python -u setup.py bdist_wheel || exit 1
+
+echo --TEST--
+python -m pytest tests || exit 1
+
+echo --COPY--
+cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
+
+echo --DOCUMENTATION--
+python -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
+if [ $? -ne 0 ]; then exit $?; fi
+
+echo --END--
+cd ..
