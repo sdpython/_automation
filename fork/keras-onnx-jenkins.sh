452df250bcb3593f8bd9995c63402eb4d1273851
xavier dupré <xavier.dupre@gmail.com>
2020-04-09 15:12:20 +0200

    Update keras-onnx-jenkins.sh

--- fork/keras-onnx-jenkins.sh
+++ fork/keras-onnx-jenkins.sh
@@ -3,7 +3,7 @@ git clone -b jenkins --single-branch https://github.com/xadupre/keras-onnx.git -
 cd keras-onnx
 
 echo --INSTALL--
-pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnx onnxruntime_dnnl onnxconverter-common || exit 1
+pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnx onnxruntime_dnnl onnxconverter-common tf2onnx || exit 1
 
 echo --TEST--
 python -m pytest tests || exit 1
