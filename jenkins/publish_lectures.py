154cb64a699c3648d0c5d3544a1534ce0824b582
xavier dupré <xavier.dupre@gmail.com>
2020-04-11 11:46:39 +0200

    update doc

--- jenkins/publish_lectures.py
+++ jenkins/publish_lectures.py
@@ -131,6 +131,7 @@ for i, mod in enumerate(sorted(modules)):
 # La fonction :func:`publish_teachings_to_web` cache beaucoup de choses.
 other_copies = [
     ('sklearn-onnx', 'sklearn-onnx-jenkins'),
+    ('keras-onnx', 'keras-onnx-jenkins'),
     ('onnxruntime', 'onnxruntime-jenkins'),
     ('onnxmltools', 'onnxmltools-jenkins'),
     ('td1a_unit_test_ci', 'td1a_unit_test_ci'),
