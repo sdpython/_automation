84f54396a082ee7e7edebce968c1b6b9cd2b9a5c
xavier dupré <xavier.dupre@gmail.com>
2020-04-14 15:36:12 +0200

    Update keras-onnx-jenkins.sh

--- fork/keras-onnx-jenkins.sh
+++ fork/keras-onnx-jenkins.sh
@@ -15,6 +15,7 @@ echo --COPY--
 cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
 
 echo --DOCUMENTATION--
+pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ onnxmltools || exit 1
 python -c "from sphinx.cmd.build import build_main;build_main(['-j2','-v','-T','-b','html','-d','build/doctrees','docs','dist/html'])" || exit 1
 if [ $? -ne 0 ]; then exit $?; fi
 
