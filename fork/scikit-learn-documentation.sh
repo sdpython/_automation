954cdac557615fa5a72846eb9d848f53b79409a8
xavier dupré <xavier.dupre@gmail.com>
2020-06-23 12:11:30 +0200

    add job

---
+++ fork/scikit-learn-documentation.sh
@@ -0,0 +1,17 @@
+echo --CLONE--
+git clone -b jenkins --single-branch https://github.com/sdpython/scikit-learn.git --recursive
+cd scikit-learn
+
+echo --BUILD--
+python -u setup.py build_ext --inplace || exit 1
+
+echo --WHEEL--
+python -u setup.py bdist_wheel || exit 1
+
+echo --doc--
+cd doc
+make
+cd ..
+
+echo --COPY--
+cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
