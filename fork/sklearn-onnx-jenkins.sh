3d8f95ee380069fbd6bca8fb4edef2499209d7d0
xavier dupré <xavier.dupre@gmail.com>
2020-12-03 18:26:33 +0100

    Update sklearn-onnx-jenkins.sh

--- fork/sklearn-onnx-jenkins.sh
+++ fork/sklearn-onnx-jenkins.sh
@@ -34,20 +34,6 @@ python -m pytest docs/tests || exit 1
 echo --COPY--
 cp dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
 
-cd benchmarks
-echo --BENCHMARK1--
-python -u bench_plot_onnxruntime_random_forest_reg.py || exit 1
-echo --BENCHMARK2--
-python -u bench_plot_onnxruntime_svm_reg.py || exit 1
-echo --BENCHMARK3--
-python -u bench_plot_onnxruntime_logreg.py || exit 1
-echo --BENCHMARK4--
-python -u bench_plot_onnxruntime_linreg.py || exit 1
-echo --BENCHMARK-GRAPH--
-cd results
-python -u post_graph.py || exit 1
-cd ../..
-
 echo --COVERAGE--
 cd tests
 export PYTHONPATH=..
