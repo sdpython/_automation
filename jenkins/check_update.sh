7d773acf8d114df5f255d76284b6ad2ea9f68f4b
xavier dupré <xavier.dupre@gmail.com>
2021-01-31 01:17:06 +0100

    update jobs

---
+++ jenkins/check_update.sh
@@ -0,0 +1,15 @@
+echo --INSTALL--
+pip install --upgrade --no-cache-dir --no-deps --index http://localhost:8067/simple/ pymyinstall || exit 1
+pip freeze
+
+echo --CHECK1--
+pymy_update numpy matplotlib seaborn statsmodels pandas jupyter tornado requests scikit-learn openpyxl
+
+echo --CHECK2--
+pymy_update jinja2 mako ujson protobuf scikit-image Pillow pyzmq xgboost lightgbm
+
+echo --CHECK3--
+pymy_update fiona cartopy descartes pyshp shapely pyproj
+
+echo --CHECK4--
+pymy_update cython pybind11 cffi numba llvmlite spacy numexpr sympy kivy cvxopt cvxpy yaml toolz cytoolz
