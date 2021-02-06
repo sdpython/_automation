11c608995b7e44c15590d402b300b1afa4861a0f
xavier dupré <xavier.dupre@gmail.com>
2021-02-06 17:41:14 +0100

    Update ortcustom-jenkins.sh

--- fork/ortcustom-jenkins.sh
+++ fork/ortcustom-jenkins.sh
@@ -17,11 +17,18 @@ pip install -r requirements-dev.txt
 echo --FREEZE--
 pip freeze
 
-echo --BUILD--
+echo --BUILD-CPP--
 cd ort-customops
 export PATH=/home/install/cmake-3.19.3-Linux-x86_64/bin:$PATH || exit 1
 cmake --version || exit 1
-bash build.sh || exit 1
+python -c "import onnxruntime;f=open('temp_ortver.txt','w');f.write('export VERORT=%s'%onnxruntime.__version__);f.close()" || exit 1
+export $(cat temp_ortver.txt) || exit 1
+bash build.sh -DONNXRUNTIME_LIB_DIR=onnxruntime-linux-x64-$VERORT/lib || exit 1
+
+echo --TEST--
+cd out/Linux
+ctest -C RelWithDebInfo
+cd ..
 
 echo --BUILD-PY--
 python setup.py build_ext --inplace || exit 1
@@ -29,7 +36,7 @@ python setup.py bdist_wheel || exit 1
 
 echo --TEST--
 export PYTHONPATH=.
-export TEST_TARGET_OPSET=12
+export TEST_TARGET_OPSET=13
 python -m pytest test --verbose --verbose || exit 1
 
 echo --WHEEL--
