1f54b5b17e8bd84d63e55ab4401c098a3fba550b
xavier dupré <xavier.dupre@gmail.com>
2021-04-15 15:44:54 +0200

    Update onnxruntime-jenkins-docker.sh

--- fork/onnxruntime-jenkins-docker.sh
+++ fork/onnxruntime-jenkins-docker.sh
@@ -10,8 +10,8 @@ git submodule update --init --recursive
 
 echo --BUILD--
 python -m pip install cibuildwheel
-set CIBW_BEFORE_BUILD=pip install pybind11 cython numpy scipy pyquickhelper scikit-learn pandas pandas_streaming onnx && python ./tools/ci_build/build.py --build_dir ./build/debian --config RelWithDebInfo --build_wheel --use_openmp --numpy_version= --skip_tests --build_shared_lib
-set CIBW_BUILD=cp39-manylinux_x86_64 cp38-macosx_x86_64
+export CIBW_BEFORE_BUILD="pip install pybind11 cython numpy scipy pyquickhelper scikit-learn pandas pandas_streaming onnx && python ./tools/ci_build/build.py --build_dir ./build/debian --config RelWithDebInfo --build_wheel --numpy_version= --skip_tests --build_shared_lib"
+export CIBW_BUILD="cp39-manylinux_x86_64 cp38-macosx_x86_64"
 python -m cibuildwheel --output-dir dist/wheelhouse --platform=linux || exit 1
 
 echo --END--
