95e72b636e33d424ede0ceaee25d5d8d8c320e25
xavier dupré <xavier.dupre@gmail.com>
2021-05-06 10:07:22 +0200

    Update onnxruntime-jenkins.sh

--- fork/onnxruntime-jenkins.sh
+++ fork/onnxruntime-jenkins.sh
@@ -22,7 +22,7 @@ python ./tools/ci_build/build.py --help
 # echo python ./tools/ci_build/build.py --build_dir ./build/debian --config Release --build_wheel --use_openmp --numpy_version= --skip-keras-test --skip_onnx_tests || exit 1
 # echo python ./tools/ci_build/build.py --build_dir ./build/debian --config Release --build_wheel --use_openmp --numpy_version= --use_dnnl --skip-keras-test --skip_onnx_tests || exit 1
 # python ./tools/ci_build/build.py --build_dir ./build/debian --config Release --build_wheel --use_openmp --numpy_version= --use_mklml --use_dnnl --skip-keras-test --skip_onnx_tests || exit 1
-python ./tools/ci_build/build.py --build_dir ./build/debian --config RelWithDebInfo --build_wheel --use_openmp --numpy_version= --skip_tests --build_shared_lib || exit 1
+python ./tools/ci_build/build.py --build_dir ./build/debian --config Release --build_wheel --numpy_version= --skip_tests --build_shared_lib || exit 1
 
 # too long
 # echo --BUILD-VALGRIND--
@@ -34,16 +34,22 @@ python ./tools/ci_build/build.py --build_dir ./build/debian --config RelWithDebI
 # valgrind ./build/debian/Release/onnxruntime_test_all "--gtest_filter=*Tree*" || exit 1
 
 echo --COPY--
-cp build/debian/RelWithDebInfo/dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server || exit 1
+cp build/debian/Release/dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server || exit 1
 
 echo --INSTALL--
 pip install tf2onnx --no-deps
-pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnxconverter-common keras2onnx || exit 1
+pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ skl2onnx onnxconverter-common || exit 1
 
-echo --DOCUMENTATION--
-cd build/debian/RelWithDebInfo
-python -m sphinx -j1 -v -T -b html -d ../../../dist/_doctrees ../../../docs/python ../../../dist/html || exit 1
+cd build/debian/Release
+
+echo --DOCUMENTATION-INFERENCE--
+python -m sphinx -j1 -v -T -b html -d ../../../dist/_doctrees ../../../docs/python/inference ../../../dist/html || exit 1
 if [ $? -ne 0 ]; then exit $?; fi
+
+echo --DOCUMENTATION-TRAINING--
+python -m sphinx -j1 -v -T -b html -d ../../../dist/_doctrees ../../../docs/python/training ../../../dist/html_training || exit 1
+if [ $? -ne 0 ]; then exit $?; fi
+
 cd ..
 cd ..
 cd ..
