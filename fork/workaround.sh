08ac4d677b6725dc5c4447b6e17fa2dec3b6e976
xavier dupré <xavier.dupre@gmail.com>
2022-01-22 18:33:24 +0100

    Update workaround.sh

--- fork/workaround.sh
+++ fork/workaround.sh
@@ -14,23 +14,19 @@ pip install --upgrade --no-cache-dir --no-deps --index http://localhost:8067/sim
 echo --aten_op_executor--
 cd ort/torch_cpp_extensions/cpu/aten_op_executor
 
-python setup.py bdist_wheel
-if [ $? -ne 0 ]; then exit $?; fi
+python setup.py bdist_wheel || exit 1
 
 echo --torch_interop_utils--
 cd ../torch_interop_utils
 
-python setup.py bdist_wheel
-if [ $? -ne 0 ]; then exit $?; fi
+python setup.py bdist_wheel || exit 1
 
 echo --COPY--
 cd ../../../..
 
-cp ort/torch_cpp_extensions/cpu/aten_op_executor/dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
-if [ $? -ne 0 ]; then exit $?; fi
+cp ort/torch_cpp_extensions/cpu/aten_op_executor/dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server || exit 1
 
-cp ort/torch_cpp_extensions/cpu/torch_interop_utils/dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server
-if [ $? -ne 0 ]; then exit $?; fi
+cp ort/torch_cpp_extensions/cpu/torch_interop_utils/dist/*.whl /var/lib/jenkins/workspace/local_pypi/local_pypi_server || exit 1
 
 echo --END--
 cd ..
