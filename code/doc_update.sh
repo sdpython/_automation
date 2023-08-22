7d0fc27b1f584dd324332089deea603d8164f927
Xavier Dupre <xadupre@microsoft.com>
2023-08-22 10:59:05 +0200

    add lecture_citation

--- code/doc_update.sh
+++ code/doc_update.sh
@@ -1,9 +1,10 @@
 clear
 
 # update sphinx
-python3 -m pip install --upgrade sphinx sphinx-gallery furo pydata-sphinx-theme
+python3 -m pip install --upgrade "sphinx<7.2" sphinx-gallery furo pydata-sphinx-theme
 
 # mlstatpy
+python3 update_doc.py lecture_citation
 python3 update_doc.py mlstatpy
 python3 update_doc.py onnx-array-api
 python3 update_doc.py onnx-extended
