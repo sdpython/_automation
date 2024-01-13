157bffb3cc6e7b4c2991d6f7516f68220d1ce21d
Xavier Dupre <xadupre@microsoft.com>
2024-01-13 13:30:11 +0100

    update doc

--- code/doc_update.sh
+++ code/doc_update.sh
@@ -3,7 +3,9 @@ clear
 # update sphinx
 export GENDOCDEFAULT="middle"
 echo "pypi update"
-python3 -m pip install --upgrade "sphinx<7.2" sphinx-gallery furo pydata-sphinx-theme > pypi-update.log
+python3 -m pip install --upgrade sphinx sphinx-gallery furo pydata-sphinx-theme > pypi-update.log
+echo "pypi freeze"
+python3 -m pip freeze > pypi-freeze.log
 
 # documentations
 if [ ! -e "lecture_citation.log" ]
